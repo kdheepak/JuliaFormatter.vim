0f08fc2ba9755ebe38076cbd6c5f6fb7c158c4ac
Dheepak Krishnamurthy <me@kdheepak.com>
2020-01-22 15:52:05 -0700

    Update server.jl

--- scripts/server.jl
+++ scripts/server.jl
@@ -32,7 +32,14 @@ function main()
             log("Formatting: ")
             log(join(text, "\n"), spacer = "\n")
             try
-                output = format_text(join(text, "\n"))
+                output = format_text(
+                    join(text, "\n"),
+                    indent = 4,
+                    margin = 92,
+                    always_for_in = false,
+                    whitespace_typedefs = false,
+                    whitespace_ops_in_indices = true,
+                )
                 data["status"] = "success"
             catch
                 log("failed")
@@ -41,7 +48,8 @@ function main()
             end
             log("\n---------------------------------------------------------------------\n")
             log(output, spacer = "\n")
-            data["params"]["text"] = [rstrip(lpad(l, length(l) + indent)) for l in split(output, "\n")]
+            data["params"]["text"] =
+                [rstrip(lpad(l, length(l) + indent)) for l in split(output, "\n")]
             print(stdout, JSON.json(data))
         end
     end
