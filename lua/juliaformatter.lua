0225edfc64dd1fe7f5d32a747a6fcc77018bc969
Dheepak Krishnamurthy <me@kdheepak.com>
2021-06-30 15:02:42 -0600

    Update lua file

--- lua/juliaformatter.lua
+++ lua/juliaformatter.lua
@@ -1,5 +1,5 @@
 local juliaformatter_sysimage = ""
-local juliaformatter_project = vim.fn.expand('<sfile>:p:h')
+local juliaformatter_project = debug.getinfo(1, "S").source:sub(2):match("(.*[/\\])"):sub(1, -2):match("(.*[/\\])")
 local formatCommand = 'julia '
 
 if vim.fn.has("win64") ~= 0 or vim.fn.has("win32") ~= 0 or vim.fn.has("win16") ~= 0 then
@@ -17,13 +17,13 @@ else
     ext = ".so"
 end
 
-local sysimage_path = juliaformatter_project .. '/scripts/juliaformatter' .. ext
+local sysimage_path = juliaformatter_project .. 'scripts/juliaformatter' .. ext
 if vim.fn.filereadable(vim.fn.expand(sysimage_path)) then
     juliaformatter_sysimage = sysimage_path
-    formatCommand = formatCommand .. '-J' .. juliaformatter_sysimage .. ' '
+    -- formatCommand = formatCommand .. '-J ' .. juliaformatter_sysimage .. ' '
 end
 
-formatCommand = formatCommand .. '--project ' .. juliaformatter_project
-                    .. [[ startup-file=no color=no -e 'using JuliaFormatter; print(format_text(String(read(stdin))))']]
+formatCommand = formatCommand .. '--project=' .. juliaformatter_project
+                    .. [[ --startup-file=no --color=no -e 'using JuliaFormatter; print(format_text(String(read(stdin))))']]
 
 return {efmConfig = {formatCommand = formatCommand, formatStdin = true}}
